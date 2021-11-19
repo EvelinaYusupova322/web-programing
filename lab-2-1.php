974d4b57569f003033d67badf2d4dd9cca6d208f
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-11-19 18:47:09 +0500

    Update lab-2-1.php
    Добавлен массив квадратов натуральных чисел

--- lab-2-1.php
+++ lab-2-1.php
@@ -1,9 +1,15 @@
 <?php
  $treug = array(1,2,3,4,5,6,7,8,9,10);
-
+ $kvd=array(1,2,3,4,5,6,7,8,9,10);
 echo'Массив треугольных чисел: ';
 foreach($treug as $k=>$n) {
     $treug[$k]=$n*($n+1)/2;
     echo $treug[$k] .'&nbsp;&nbsp;';
    }
+echo '<br>';
+ echo'Массив квадратов натуральных чисел: ';
+ foreach($kvd as $a=>$b) {
+    $kvd[$a]=$b*$b;
+    echo $kvd[$a].'&nbsp;';
+    }
 ?>
