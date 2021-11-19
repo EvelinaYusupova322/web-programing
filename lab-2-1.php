d3966904f21f3dc65f06806f67dc7a818b97bdd7
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-11-19 18:52:55 +0500

    Объединены два массива

--- lab-2-1.php
+++ lab-2-1.php
@@ -1,6 +1,7 @@
 <?php
  $treug = array(1,2,3,4,5,6,7,8,9,10);
  $kvd=array(1,2,3,4,5,6,7,8,9,10);
+ $rez=array();
 echo'Массив треугольных чисел: ';
 foreach($treug as $k=>$n) {
     $treug[$k]=$n*($n+1)/2;
@@ -12,4 +13,8 @@ echo '<br>';
     $kvd[$a]=$b*$b;
     echo $kvd[$a].'&nbsp;';
     }
+echo '<br>';
+ echo'Объединенный массив: ';
+ $rez=array_merge($treug,$kvd);
+ print_r($rez);
 ?>
