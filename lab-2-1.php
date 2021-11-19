126c1ab3878791f942c05a5ac8434e3009790c50
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-11-19 18:41:16 +0500

    Update lab-2-1.php
    Изменение кода

--- lab-2-1.php
+++ lab-2-1.php
@@ -1,7 +1,7 @@
 <?php
  $treug = array(1,2,3,4,5,6,7,8,9,10);
 
-echo'massiv treugolnuh chisel: ';
+echo'Массив треугольных чисел: ';
 foreach($treug as $k=>$n) {
     $treug[$k]=$n*($n+1)/2;
     echo $treug[$k] .'&nbsp;&nbsp;';
