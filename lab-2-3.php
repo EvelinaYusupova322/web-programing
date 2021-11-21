cf02164a50c703d33b763f68edee25232acf1762
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-11-21 22:07:18 +0500

    Добавление ключа rating в массив

--- lab-2-3.php
+++ lab-2-3.php
@@ -2,4 +2,9 @@
 echo 'Ассоциативный массив: ';
 $cust= array("cnum"=>"2001", "cname"=>"Hoffman", "city"=>"London", "snum"=>"1001");
 print_r ($cust);
+
+echo '<br><br>' . 'Добавлен ключ rating со значением 100 в массив: ';
+$cust["rating"] = 100;
+foreach($cust as $k => $v)
+echo  $k, ' => ', $v;
 ?>
