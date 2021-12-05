faf64140e67a894698d043468d0b14d8bc0b2194
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-12-05 16:11:06 +0500

    Добавлен файл с ссылками для ЛР№3

---
+++ index3.php
@@ -0,0 +1,14 @@
+<u1>
+        <li><a href="http://phpYusupova.bb/www/LR3/lab-3-1.html">lab-3-1</a></li>
+        <li><a href="http://phpYusupova.bb/www/LR3/lab-3-2.php">lab-3-2</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/lab-3-3.php">lab-3-3</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/lab-3-4.php">lab-3-4</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/lab-3-5.php">lab-3-5</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/rez.php">rez</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/lab-3-6.2.php">lab-3-6.2</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/prov.php">prov</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/lab-3-6.5.php">lab-3-6.5</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/out.php">out</a></li>
+		<li><a href="http://phpYusupova.bb/www/LR3/lab-3-6.8.php">lab-3-6.8</a></li>
+
+</u1>
