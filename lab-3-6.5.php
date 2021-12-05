6ebe731dd0f2d68f2b3b1d94c862ab260a3de8fd
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-12-05 16:04:45 +0500

    Добавлено решение задачи 3-6 (5)

---
+++ lab-3-6.5.php
@@ -0,0 +1,14 @@
+﻿<html>
+<head>
+<title>Юсупова Э.А. ПИ-322</title>
+<h2>Вывести в столбец список слов, которые входят в данное</h2>
+</HEAD>
+<BODY>
+<FORM method="post" action="/www/LR3/out.php">
+ <p>Введите какой-либо отрывок текста: <br>
+ <INPUT type="text" name="a" size="100">
+ <input type ="submit" value="Вывести">
+ <input type ="reset" value ="Очистить">
+</FORM>
+</BODY> 
+</HTML>
