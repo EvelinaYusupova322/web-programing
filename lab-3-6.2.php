81e80d26b9c3a2eb3db373ddbbd0420416dce2fe
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-12-05 16:02:31 +0500

    Добавлено решение задачи 3-6 (2)

---
+++ lab-3-6.2.php
@@ -0,0 +1,16 @@
+﻿<html>
+<head>
+<title>Юсупова Э.А. ПИ-322</title>
+<h2>Проверить, можно ли из букв, входящих в слово А, составить слово В</h2>
+</HEAD>
+<BODY>
+<FORM method="post" action="/www/LR3/prov.php">
+ <p>Введите слово "А": <br>
+ <INPUT type="text" name="a" size="30">
+  <p>Введите слово "B": <br>
+ <INPUT type="text" name="b" size="30"><br><br>
+ <input type ="submit" value="Проверить">
+ <input type ="reset" name="ochistit" value ="Очистить" >
+</FORM>
+</BODY> 
+</HTML>
