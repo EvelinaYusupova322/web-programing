a3ecf456773e6ee19b8cad4400bffb32d625509f
EvelinaYusupova322 <93489626+EvelinaYusupova322@users.noreply.github.com>
2021-12-20 10:37:44 +0500

    Delete index2.php

--- index2.php
+++
@@ -1,8 +0,0 @@
-<u1>
-        <li><a href="http://phpYusupova.bb/www/LR2/lab-2-1.php">lab-2-1</a></li>
-		<li><a href="http://phpYusupova.bb/www/LR2/lab-2-2.php">lab-2-2</a></li>
-		<li><a href="http://phpYusupova.bb/www/LR2/lab-2-3.php">lab-2-3</a></li>
-		<li><a href="http://phpYusupova.bb/www/LR2/lab-2-4.php">lab-2-4</a></li>
-        <li><a href="http://phpYusupova.bb/www/LR2/lab-2-5.php">lab-2-5</a></li>
-		
-</u1>
